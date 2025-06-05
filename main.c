57b9493516eb9fddce45b4460a2ea654a08ddf37
Aryan <aryanatif01@gmail.com>
2025-06-05 17:09:19 +0500

    Change the welcome message format

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(){
     printf("===========================================================\n");
-    printf("             Welcome to the Cipher Machine\n");
+    printf("             Welcome to the Cipher Machine                 \n");
     printf("===========================================================\n\n");
 
     return 0;
