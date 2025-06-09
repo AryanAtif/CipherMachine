71b09254275b2d8441a5bb9e975f71a092e6058b
Aryan <aryanatif01@gmail.com>
2025-06-09 12:32:52 +0500

    main.c

--- main.c
+++ main.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+// CAESAR STRUCTURE...
+typedef struct 
+{
+    char text[1000];
+    int key;
+} caesar;
+
 
 int main(){
     printf("===========================================================\n");
     printf("             Welcome to the Cipher Machine                 \n");
     printf("===========================================================\n\n");
 
+
+
+    // the caesar code
+    caesar s1;
+
+
+    printf("Enter the plaintext: ");
+    fgets(s1.text, 1000, stdin);
+
+    printf("The text you entered is: %s", s1.text);
+    
+    
+    
+    getchar();
     return 0;
 }
